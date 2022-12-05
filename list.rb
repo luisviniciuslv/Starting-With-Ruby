3fb8db07ba5a992d37cd20519ce7e173b238be39
Luis Vinicius <luisvpersy@gmail.com>
2022-12-05 18:34:03 -0300

    list

---
+++ list.rb
@@ -0,0 +1,12 @@
+list = []
+
+# list << 3 == list.push(3) or list.append(3)
+
+list << 3 # list = [3]
+list << 4 # list = [3, 4]
+
+# list.size == count how many values ​​are in the array
+
+list.size # list.size == 2
+
+puts list.size
